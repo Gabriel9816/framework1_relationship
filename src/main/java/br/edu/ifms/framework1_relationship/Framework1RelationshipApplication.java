caabe0076bccca32464d8e776384cce2bb1f9a78
Gabriel9816 <gabriel.oliveira6@estudante.ifms.edu.br>
2022-05-21 09:56:55 -0400

    first commit

---
+++ src/main/java/br/edu/ifms/framework1_relationship/Framework1RelationshipApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.framework1_relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Framework1RelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Framework1RelationshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifms/framework1_relationship/Framework1RelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.framework1_relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Framework1RelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
